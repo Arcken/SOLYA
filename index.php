1afd120c507190e2577d8ddc1f1c2e7fa035d296
LEGENDRE Patrick <legendre.patrick@dev-gen.fr>
2015-09-07 16:42:19 +0200

    t

--- index.php
+++ index.php
@@ -1,41 +1,4 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<?php
-session_start(); //lance la session
-require_once('inc/ini.inc'); //recupere parametre du fichier param.ini
-require_once 'controler/control.php'; //appel page du contrôleur
-require_once('inc/model.inc'); //recupere parametre du fichier param.ini
-// Initialisation des variables
 
-?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title><?php echo $sTitre ?></title>
-    </head>
-    <body>
-        <?php
-                        
-        if (isset($_SESSION['auth']) && $_SESSION['auth'] == TRUE)
-            require 'view/view_menu.php';
-        else 
-            require 'view/view_connection.php';
-     //require 'view/view_connexion.php'
-        ?>
-    </body>
-</html>
-=======
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <meta charset="UTF-8"></meta>
 
 <?php
@@ -45,4 +8,4 @@ and open the template in the editor.
 
 <title><?php echo $sTitle; ?></title>
      
->>>>>>> origin/master
+
