859c814e558df24906b38363cc51f70707da94df
Arcken <dev.richard.olivier@gmail.com>
2015-09-07 10:47:48 +0200

    Premier commit
    Premier commit de l'application

---
+++ view/view_connexion.php
@@ -0,0 +1,8 @@
+
+
+<link type="text/css" href="css/style_connexion.css" rel="stylesheet" >
+<form class="cnx" action = "index.php" method ="get" >
+    <input type="text" require="required" name="pseudo"></input>
+    <input type="password" require="required" name="pwd"></input>
+    <input type="submit" name ="connexion" value="Connexion"></input>
+</form>
---
+++ view/view_menu.php
@@ -0,0 +1,117 @@
+<!-- Menu de l'application -->
+
+<link type="text/css" href="css/style_menu.css" rel="stylesheet" >
+<div id='cssmenu'>
+<ul>
+   <li><a href='index.html'><span>Accueil</span></a></li>
+   <li><a href='#'><span>Contacts</span></a>
+      <ul>
+         <li><a href='#'><span>Créer</span></a>
+            <ul>
+               <li><a href='#'><span>Fournisseur</span></a></li>
+               <li><a href='#'><span> Client</span></a></li>
+               <li><a href='#'><span>Prospect</span></a></li>
+            </ul>
+         </li>
+         <li><a href='#'><span>Consulter</span></a>
+            <ul>
+               <li><a href='#'><span>Fournisseur</span></a></li>
+               <li><a href='#'><span>Client</span></a></li>
+               <li class='last'><a href='#'><span>Prospect</span></a></li>
+            </ul>
+         </li>
+      </ul>
+   </li>
+   <li><a href='#'><span>Stock</span></a>
+      <ul>
+         <li><a href='#'><span>Créer</span></a>
+            <ul>
+               <li><a href='#'><span>Entrée</span></a></li>
+               <li><a href='#'><span>Sortie</span></a></li>
+               <li><a href='#'><span>Retour</span></a></li>
+               <li><a href='#'><span>Inventaire</span></a></li>
+            </ul>
+         </li>
+         <li><a href='#'><span>Consulter</span></a>
+            <ul>
+               <li><a href='#'><span>Entrée</span></a></li>
+               <li><a href='#'><span>Sortie</span></a></li>
+               <li><a href='#'><span>Retour</span></a></li>
+               <li><a href='#'><span>Inventaire</span></a></li>
+            </ul>
+         </li>
+      </ul>
+   </li>
+   <li><a href='#'><span>Catalogue</span></a>
+      <ul>
+         <li><a href='#'><span>Créer</span></a>
+            <ul>
+               <li><a href='#'><span>Gamme</span></a></li>
+               <li><a href='#'><span>Fiche Article</span></a></li>
+               <li class='last'><a href='#'><span>Références</span></a></li>
+            </ul>
+         </li>
+         <li><a href='#'><span>Consulter</span></a>
+            <ul>
+               <li><a href='#'><span>Gamme</span></a></li>
+               <li><a href='#'><span>Fiche Article</span></a></li>
+               <li><a href='#'><span>Références</span></a></li>
+            </ul>
+         </li>
+      </ul>
+   </li>
+   <li><a href='#'><span>Achats</span></a>
+      <ul>
+         <li><a href='#'><span>Créer</span></a>
+            <ul>
+               <li><a href='#'><span>Commande</span></a></li>
+               <li><a href='#'><span>Facture</span></a></li>
+               <li class='last'><a href='#'><span>Paiement</span></a></li>
+            </ul>
+         </li>
+         <li><a href='#'><span>Consulter</span></a>
+            <ul>
+               <li><a href='#'><span>Commande</span></a></li>
+               <li><a href='#'><span>Facture</span></a></li>
+               <li class='last'><a href='#'><span>Paiement</span></a></li>
+            </ul>
+         </li>
+      </ul>
+   </li>
+   <li><a href='#'><span>Ventes</span></a>
+      <ul>
+         <li><a href='#'><span>Créer</span></a>
+            <ul>
+               <li><a href='#'><span>Commande</span></a></li>
+               <li><a href='#'><span>Devis</span></a></li>
+               <li><a href='#'><span>Facture</span></a></li>
+               <li class='last'><a href='#'><span>Encaissement</span></a></li>
+            </ul>
+         </li>
+         <li><a href='#'><span>Consulter</span></a>
+            <ul>
+               <li><a href='#'><span>Commande</span></a></li>
+               <li><a href='#'><span>Devis</span></a></li>
+               <li><a href='#'><span>Facture</span></a></li>
+               <li><a href='#'><span>Encaissement</span></a></li>
+            </ul>
+         </li>
+      </ul>
+   </li>
+   <li><a href='#'><span>Statistiques</span></a>
+      <ul>
+         <li class='has-sub'><a href='#'><span>Consulter</span></a>
+            <ul>
+               <li><a href='#'><span>Annuel</span></a></li>
+               <li><a href='#'><span>Mensuel</span></a></li>
+            </ul>
+         </li>
+      </ul>
+   </li>
+   <li><a href='#'><span>Alertes</span></a></li>
+</ul>
+</div>
+</div>
+
+
+
