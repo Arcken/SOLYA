7188bf07d473c2527d6baa89b422049ef8a36388
Arcken <dev.richard.olivier@gmail.com>
2015-09-07 11:31:25 +0200

    Ajout exception
    Fichier MySqlException pour affichage des erreurs

---
+++ exception/MySQLException.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Gestion des erreurs avec les exceptions
+ */
+class MySQLException extends Exception {
+
+    private $cnx = "";
+
+    public function __construct($Msg, $cnx) {
+        parent :: __construct($Msg);
+        $this->cnx = $cnx;
+    }
+
+    /**
+     * Retourne le message, la ligne et le fichier d'origine de l'erreur
+     * 
+     * @return string
+     */
+    public function RetourneErreur() {
+        $msg = '<div><strong>' . $this->getMessage() . '</strong>';
+        if (isset($this->cnx) && $this->cnx !== null) {
+            $msg .= '<br>' . $this->cnx->errorInfo()[0] ;
+        }
+        $msg .= '<br>Trace : ' . str_replace("#", "<br>", $this->getTraceAsString());
+//        $msg .= ' dans ' . $this->getFile();
+//        $msg .= ' ligne : ' . $this->getLine();
+        $msg .= '</div>';
+        return $msg;
+    }
+
+}
+
+?>
