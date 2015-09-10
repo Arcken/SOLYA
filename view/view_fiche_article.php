c93cc7c5e7caff7a1791f413905376f4ecc480ce
LEGENDRE Patrick <legendre.patrick@dev-gen.fr>
2015-09-10 15:02:20 +0200

    css
    apparence formulaire

--- view/view_fiche_article.php
+++ view/view_fiche_article.php
@@ -4,51 +4,47 @@
     <form class="form" action="index.php" method="get">
         <div class="gauche">
             <div> 
-                <label for="fiartId"> Code de la fiche article </label>
+                <label for="fiartId"> Code de la fiche article </label><br>
                 <input name="fiartId" placeholder="Abréviation pays-gammes" required type="text">
                 <img src="img/icon/accept.png" alt=""/>
                 <br>
-                <label for="fiartLbl"> Libellé de la fiche article </label>
+                <label for="fiartLbl"> Libellé de la fiche article </label><br>
                 <input name="fiartLbl" placeholder="description" required type="text">
                 <br>
-                <label for="gamme"> Gamme: </label>
+                <label for="gamme"> Gamme: </label><br>
                 <select name="gamme">
                     <option value="1">Sirop</option>
                     <option value="2">Tablette</option>
                     <option value="2">Graine</option>                        
                 </select>
-                <a href="" onClick="popup('view_gamme.php');">
-                    <img src="img/icon/add.png" alt=""/>
+                <a href="" onClick="popup('view/view_gamme.php');">
+                    <img src="img/icon/add.png" alt="" title="Créer gamme"/>
                 </a>
                 <br>
-                <label for="pays"> Pays: </label>
+                <label for="pays"> Pays: </label><br>
                 <select name="pays">
                     <option value="1">Mexique</option>
                     <option value="2">France</option>
                     <option value="2">Angleterre</option>                        
                 </select>
                 <a href="" onClick="popup('view_pays.php');">
-                    <img src="img/icon/add.png" alt=""/>
+                    <img src="img/icon/add.png" alt="" title="Créer pays"/>
                 </a>
                 <br>
             </div>
 
             <div>
-                <div
-                    <label for="fiartIng"> Ingrédients: </label>                    
+                <div>
+                    <label for="fiartIng"> Ingrédients: </label><br>               
                     <textarea name="fiartIng" rows="4" cols="30" placeholder="Saisie"></textarea>
                     <br>
                 </div>
                 <div>                    
-                    <label for="fiartAlg"> Allergénes: </label>
+                    <label for="fiartAlg"> Allergénes: </label><br>
                     <textarea name="fiartAlg" rows="4" cols="30" placeholder="Saisie"></textarea>
                 </div>
             </div>
-            <div>
-                <input name="btnForm" type="submit" value="<?php echo $sButton;?>">
-                <input name="clear" type="reset"> 
-                <input name="action" value="<?php echo $sAction ?>" type="text">
-            </div>
+            
         </div>
         <div class="droite">
             <p> Table de nutrition: </p>
@@ -60,6 +56,10 @@
             </a>
             <br>
         </div>
-
+        <div class="bas">
+                <input name="btnForm" type="submit" value="<?php echo $sButton;?>">
+                <input name="clear" type="reset"> 
+                <input name="action" value="<?php echo $sAction ?>" type="text" hidden>
+            </div>
     </form>
 </div>
