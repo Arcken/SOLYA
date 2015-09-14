4013cacf43884db80a961b996658e1e83a07b040
LEGENDRE Patrick <legendre.patrick@dev-gen.fr>
2015-09-14 11:26:24 +0200

    Ajout ajax
    Select des gammes

--- ajax/getGamme.php
+++ ajax/getGamme.php
@@ -1,8 +1,8 @@
 <?php
+$tab = array();
+$requete = "SELECT * FROM GAMME";
+$resultat = $bdd->query($requete);
+$donnees = $resultat->fetch(PDO::FETCH_ASSOC);
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+echo json_encode($tab);
+?>
